a6ad0c836d7c8243ac6473a618d04aaedda7de3b
Elias Joseph <eljoseph@amd.com>
2024-12-12 22:14:11 +0000

    created backend application

---
+++ demo/src/backend/listener.py
@@ -0,0 +1,165 @@
+from github import Github
+import datetime, time
+import numpy as np
+import pandas as pd
+from flask import Flask, request, jsonify
+import tqdm
+import pickle
+import os
+import sqlite3
+import argparse
+
+class Dashboard:
+
+    def __init__(self, key, repo, db_file, port=5000):
+        self.key = key
+        self.repo_path = repo
+        self.github = Github(self.key)
+        self.repo = self.github.get_repo(self.repo_path)
+        self.app = Flask(__name__)
+        self.app.add_url_rule(
+            "/webhook", "webhook", self.handle_webhook, methods=["POST"]
+        )
+        self.db_file = db_file
+        self.port = port
+
+    def start(self):
+        self.app.run(port=self.port, debug=True)
+
+    def stop(self):
+        pass
+
+    def handle_webhook(self):
+        data = request.get_json()
+        # handle new branch creation
+        if data.get("ref_type") == "branch":
+            self.add_branch(data)
+        # handle new commit
+        if "commits" in data:
+            self.add_commit(data)
+        # handle new workflow run
+        if data.get("action") == "completed" and "workflow_run" in data:
+            self.add_workflow_run(data)
+        return "", 200
+
+    def add_commit(self, data):
+        conn = sqlite3.connect(self.db_file)
+        c = conn.cursor()
+        c.execute("PRAGMA busy_timeout = 5000;")
+        print("ADDING COMMIT")
+        branch_name = data.get("ref").replace("refs/heads/", "")
+        pusher = data.get("pusher", {}).get("name")
+        for commit in data.get("commits", []):
+            commit_hash = commit.get("id")
+            author = data.get("author", {}).get("name")
+            message = commit.get("message")
+            c.execute(
+                "INSERT OR REPLACE INTO commits (hash, author, message) VALUES (?, ?, ?)",
+                (commit_hash, author, message),
+            )
+        conn.commit()
+        conn.close()
+
+    def add_branch(self, data):
+        conn = sqlite3.connect(self.db_file)
+        c = conn.cursor()
+        c.execute("PRAGMA busy_timeout = 5000;")
+        print("ADDING BRANCH")
+        branch_name = data.get("ref")
+        author = data.get("sender", {}).get("login")
+        c.execute(
+            "INSERT OR REPLACE INTO branches (name, author) VALUES (?, ?)",
+            (branch_name, author),
+        )
+        conn.commit()
+        conn.close()
+
+    def add_workflow_run(self, data):
+        conn = sqlite3.connect(self.db_file)
+        c = conn.cursor()
+        c.execute("PRAGMA busy_timeout = 5000;")
+        print("ADDING WORKFLOW RUN")
+        workflow_run = data.get("workflow_run", {})
+        workflow_name = workflow_run.get("name")
+        branch_name = workflow_run.get("head_branch")
+        commit_hash = workflow_run.get("head_sha")
+        author = workflow_run.get("actor", {}).get("login")
+        conclusion = workflow_run.get("conclusion")
+        status = workflow_run.get("status")
+        run_id = workflow_run.get("id")
+        run_url = workflow_run.get("html_url")
+        created_at_dt = datetime.datetime.fromisoformat(
+            workflow_run.get("created_at").replace("Z", "")
+        )
+        updated_at_dt = datetime.datetime.fromisoformat(
+            workflow_run.get("updated_at").replace("Z", "")
+        )
+        started_at_dt = datetime.datetime.fromisoformat(
+            workflow_run.get("run_started_at").replace("Z", "")
+        )
+        runtime = (updated_at_dt - created_at_dt).total_seconds()
+        try:
+            queue_time = (started_at_dt - created_at_dt).total_seconds()
+        except:
+            queue_time = (updated_at_dt - created_at_dt).total_seconds()
+        created_at = time.mktime(created_at_dt.timetuple())
+        updated_at = time.mktime(updated_at_dt.timetuple())
+        started_at = time.mktime(started_at_dt.timetuple())
+        c.execute("SELECT id FROM branches WHERE name = ?", (branch_name,))
+        try:
+            branch_id = c.fetchone()[0]
+        except:
+            print(f"No branch id found for {branch_name}")
+            branch_id = -1
+        c.execute("SELECT id FROM commits WHERE hash = ?", (commit_hash,))
+        try:
+            commit_id = c.fetchone()[0]
+        except:
+            print(f"No commit id found for {commit_hash}")
+            commit_id = -1
+        c.execute("SELECT id FROM workflows WHERE name = ?", (workflow_name,))
+        try:
+            workflow_id = c.fetchone()[0]
+        except:
+            print(f"No workflow id found for {workflow_name}")
+            workflow_id = -1
+        c.execute(
+            "INSERT OR REPLACE INTO workflowruns (branch, commitid, workflow, author, runtime, createtime, starttime, endtime, queuetime, status, conclusion, url, gitid, archivedbranchname, archivedcommithash, archivedworkflowname) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+            (
+                branch_id,
+                commit_id,
+                workflow_id,
+                author,
+                runtime,
+                created_at,
+                started_at,
+                updated_at,
+                queue_time,
+                status,
+                conclusion,
+                run_url,
+                run_id,
+                branch_name,
+                commit_hash,
+                workflow_name,
+            ),
+        )
+        conn.commit()
+        conn.close()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(prog="Backend-Listener",
+                                     description="starts the listener to live update the database")
+    parser.add_argument('-db', '--database', help="database file in .db format")
+    parser.add_argument('-r', '--repo', help="repository to scrape data from")
+    parser.add_argument('-k', "--key", help="repository key")
+    parser.add_argument('-p', "--port", help="port to expose", default=5000)
+    args = parser.parse_args()
+    dashboard = Dashboard(
+        args.key,
+        args.repo,
+        args.database,
+        args.port
+    )
+    dashboard.start()
---
+++ demo/src/backend/local-database.py
@@ -0,0 +1,182 @@
+import sqlite3
+import os
+from github import Github
+import datetime, time
+import argparse
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(prog="Local-Database",
+                                     description="Initialize Local DataBase")
+    parser.add_argument('-db', '--database', help="database file in .db format")
+    parser.add_argument('-r', '--repo', help="repository to scrape data from")
+    parser.add_argument('-k', "--key", help="repository key")
+    args = parser.parse_args()
+    conn = sqlite3.connect(args.database)
+    c = conn.cursor()
+    c.execute("PRAGMA foreign_keys = ON;")
+
+    c.execute(
+        """
+            CREATE TABLE branches (
+                id INTEGER PRIMARY KEY, --Auto-incrementing primary key
+                name TEXT UNIQUE NOT NULL,
+                author TEXT
+            )
+            """
+    )
+    conn.commit()
+
+    c.execute(
+        """
+            CREATE TABLE commits (
+                id INTEGER PRIMARY KEY, --Auto-incrementing primary key
+                hash TEXT UNIQUE NOT NULL,
+                author TEXT,
+                message TEXT
+            )
+            """
+    )
+    conn.commit()
+
+    c.execute(
+        """
+            CREATE TABLE workflows (
+                id INTEGER PRIMARY KEY, --Auto incrementing primary key
+                name TEXT UNIQUE NOT NULL,
+                url TEXT NOT NULL
+            )
+            """
+    )
+    conn.commit()
+
+    c.execute(
+        """
+            CREATE TABLE workflowruns (
+                id INTEGER PRIMARY KEY, --Auto incrementing primary key
+                branch INTEGER NOT NULL, --Foreign key column
+                commitid INTEGER NOT NULL, --Foreign key column
+                workflow INTEGER NOT NULL, --Foreign key column
+                author TEXT,
+                runtime REAL DEFAULT 0.0,
+                createtime REAL,
+                starttime REAL,
+                endtime REAL,
+                queuetime REAL DEFAULT 0.0,
+                status TEXT,
+                conclusion TEXT,
+                url TEXT,
+                gitid INT UNIQUE,
+                archivedbranchname TEXT,
+                archivedcommithash TEXT,
+                archivedworkflowname TEXT
+            )
+            """
+    )
+    conn.commit()
+
+    print("POPULATING DATABASE")
+    github = Github(args.key)
+    repo = github.get_repo(args.repo)
+
+
+    def get_workflow_run_row(workflow_run, c):
+        branch = workflow_run.head_branch
+        c.execute("SELECT id FROM branches WHERE name = ?", (branch,))
+        try:
+            branch_id = c.fetchone()[0]
+        except:
+            print(f"No branch id found for {branch}")
+            branch_id = -1
+        commit = workflow_run.head_sha
+        c.execute("SELECT id FROM commits WHERE hash = ?", (commit,))
+        try:
+            commit_id = c.fetchone()[0]
+        except:
+            print(f"No commit id found for {commit}")
+            commit_id = -1
+        workflow_name = workflow_run.name
+        c.execute("SELECT id FROM workflows WHERE name = ?", (workflow_name,))
+        try:
+            workflow_id = c.fetchone()[0]
+        except:
+            print(f"No workflow id found for {workflow_name}")
+            workflow_id = -1
+        url = workflow_run.url
+        gitid = workflow_run.id
+        author = workflow_run.actor.login
+        status = workflow_run.status
+        conclusion = workflow_run.conclusion
+        createtime = time.mktime(workflow_run.created_at.timetuple())
+        starttime = time.mktime(workflow_run.run_started_at.timetuple())
+        endtime = time.mktime(workflow_run.updated_at.timetuple())
+        queuetime = starttime - createtime
+        runtime = endtime - starttime
+        return (
+            branch_id,
+            commit_id,
+            workflow_id,
+            author,
+            runtime,
+            createtime,
+            starttime,
+            endtime,
+            queuetime,
+            status,
+            conclusion,
+            url,
+            gitid,
+            branch,
+            commit,
+            workflow_name,
+        )
+
+
+    print("POPULATING BRANCHES")
+
+    branches = repo.get_branches()
+    branch_values = [(branch.name,) for branch in branches]
+    c.executemany(
+        "INSERT OR REPLACE INTO branches (name) VALUES (?)", branch_values
+    )
+    conn.commit()
+
+    print("POPULATING COMMITS")
+
+    commits = repo.get_commits()
+    commit_values = [
+        (str(commit.sha), commit.commit.author.name, commit.commit.message)
+        for commit in commits
+    ]
+    c.executemany(
+        "INSERT OR REPLACE INTO commits (hash, author, message) VALUES (?, ?, ?)",
+        commit_values,
+    )
+    conn.commit()
+
+    print("POPULATING WORKFLOWS")
+
+    workflows = repo.get_workflows()
+    workflow_values = [(workflow.name, workflow.url) for workflow in workflows]
+    c.executemany(
+        "INSERT OR REPLACE INTO workflows (name, url) VALUES (?, ?)",
+        workflow_values,
+    )
+    conn.commit()
+
+    print("POPULATING WORKFLOW RUNS")
+
+    workflow_runs = repo.get_workflow_runs()
+    workflow_run_values = [
+        get_workflow_run_row(workflow_run, c) for workflow_run in workflow_runs
+    ]
+    c.executemany(
+        "INSERT OR REPLACE INTO workflowruns (branch, commitid, workflow, author, runtime, createtime, starttime, endtime, queuetime, status, conclusion, url, gitid, archivedbranchname, archivedcommithash, archivedworkflowname) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+        workflow_run_values,
+    )
+    conn.commit()
+
+    if __name__ == "__main__":
+        print("TESTING DB")
+
+    conn.commit()
+    conn.close()
